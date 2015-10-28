d276740df06068714f75bda67d4932e9e4cfecb7
Christopher Azar <christopherazar@gmail.com>
2015-10-27 18:49:00 -0700

    Add variables to count the resize count of the window

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,28 @@ var menu = $('.menu-content');
 window.onload = function() { document.getElementById('menu').style.display = 'none'; };
 
 
-// Resize easter egg
 // Resize the window, I dare ya :)
+
+var resizeCount = 0; // Set reset count initially 0
+
 $(window).resize(function() {
     if(this.resizeTO) clearTimeout(this.resizeTO);
     this.resizeTO = setTimeout(function() {
-        $(this).trigger('resizeEnd');
+        $(this).trigger('resizeEnd'); // When they stop resizing plz
+        resizeCount++; // Take note and add one to resizeCount
+        // console.log(resizeCount);
     }, 500);
+
 });
 
+// If they've resized the window, they'll see a message
 $(window).on('resizeEnd', function() {
-    console.log("Oh yeah, it's responsive. 😎");
-    console.log("Curious? Email me: christopherazar@gmail.com");
-    $(this).off();
+    if (resizeCount == 0) { // Increase this number if you want to make 'em work for it
+        console.log("Oh yeah, it's responsive. 😎");
+        console.log("Curious? Email me: christopherazar@gmail.com");
+    } else if (resizeCount >= 5) {
+        console.log("Super fun, right?");
+        $(this).off(); // That's all folks
+    } else {}
+
 });
