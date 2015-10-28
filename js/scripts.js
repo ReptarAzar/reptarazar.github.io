a4d1e742b075371a039b18372a0791fd5437c070
Christopher Azar <christopherazar@gmail.com>
2015-10-27 17:42:36 -0700

    Put the JS snippets in a single file. Added a responsive easter egg.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// Hide the nav
+var menu = $('.menu-content');
+    $(menu).hide();
+    $( ".menu" ).click(function() {
+        $( ".menu-content" ).slideToggle( 150 );
+    });
+
+window.onload = function() { document.getElementById('menu').style.display = 'none'; };
+
+
+// Resize easter egg
+$(window).resize(function() {
+    if(this.resizeTO) clearTimeout(this.resizeTO);
+    this.resizeTO = setTimeout(function() {
+        $(this).trigger('resizeEnd');
+    }, 500);
+});
+
+$(window).on('resizeEnd', function() {
+    console.log("Oh yeah, it's responsive. 😎");
+    console.log("Curious? Email me: christopherazar@gmail.com");
+    $(this).off();
+
+});
